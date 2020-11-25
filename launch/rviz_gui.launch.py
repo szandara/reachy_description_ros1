fe8d93eae6d0f8c6b69795c05f7031ee2f90a61e
Pierre Rouanet <pierre.rouanet@gmail.com>
2020-11-25 16:40:13 +0100

    Add launcher without gui.

--- launch/rviz_gui.launch.py
+++ launch/rviz_gui.launch.py
@@ -25,8 +25,8 @@ def generate_launch_description():
             arguments=[urdf],
         ),
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
         ),
         Node(
             package='rviz2',
